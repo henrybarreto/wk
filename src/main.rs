edf46c8bbcbdf4f5fe32236003874b1b0b18e481
Henry Barreto <me@henrybarreto.dev>
2022-06-04 19:10:46 -0300

    add unimplemented workspace structure and check CLI command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use clap::{App, Arg};
+use clap::{Arg, Command};
 
 mod configuration;
+mod workspace;
 
 fn main() {
-    let app = App::new("wk")
+    let command = Command::new("wk")
         .version("0.1.0")
         .about("WK is a CLI tool to create, manager and access workspaces")
         .author("Henry Barreto <me@henrybarreto.dev>")
@@ -17,6 +18,7 @@ fn main() {
             .help("Save a workspace")
             .takes_value(true)
             .value_names(&["NAME", "PATH"])
+            .takes_value(true)
             .exclusive(true)
     )
     .arg(
@@ -27,8 +29,17 @@ fn main() {
             .exclusive(true)
     );
 
-    let matches = app.get_matches();
-    println!("{:?}", matches);
-
-    unimplemented!();
+    // TODO: Deal with panic errors.
+    let matches = command.get_matches();
+    if matches.is_present("workspace") {
+        let workspace = matches.value_of("workspace").unwrap();
+        workspace::Workspace::go(workspace)
+    } else if matches.is_present("save") {
+        let save = matches.values_of("save").unwrap().into_iter().collect::<Vec<&str>>();
+        workspace::Workspace::save(save[0], save[1])
+    } else if matches.is_present("list") {
+        workspace::Workspace::list()
+    } else {
+        println!("{:#?}", matches)
+    }
 }
---
+++ src/workspace.rs
@@ -0,0 +1,23 @@
+// TODO: Add reponse errors.
+
+pub struct Workspace {
+}
+
+impl Workspace {
+    pub fn new() -> Workspace {
+        Workspace {
+        }
+    }
+
+    pub fn go(workspace: &str) {
+        unimplemented!()
+    }
+
+    pub fn save(workspace: &str, path: &str) {
+        unimplemented!()
+    }
+
+    pub fn list() {
+        unimplemented!()
+    }
+}
